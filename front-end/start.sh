e1c6c026d99e63f654d97900ff3a80a2e1136797
Ronan Manoj <ronmantech@gmail.com>
2020-05-07 23:29:27 -0700

    included front end application server code

---
+++ front-end/start.sh
@@ -0,0 +1,9 @@
+
+echo "Starting ElasticSearch Server"
+/Users/ronanmanoj/Documents/elasticsearch-7.5.2/bin/elasticsearch
+
+echo "Starting Kibana Server"
+/Users/ronanmanoj/Documents/kibana-7.5.2-darwin-x86_64/bin/kibana
+
+echo "Starting Postgres Test Database"
+psql test
